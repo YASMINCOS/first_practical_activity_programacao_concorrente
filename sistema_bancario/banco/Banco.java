14dcaecacca74700e4ae820d04b17a52ab993175
YASMINCOS <costayasmin367@gmail.com>
2024-04-16 13:47:35 -0300

    adicionando opersacoes iniciais na class Conta com o conceito de lock, aplicando os metodos lock e unlock

--- sistema_bancario/banco/Banco.java
+++ sistema_bancario/banco/Banco.java
@@ -1,5 +1,23 @@
 package sistema_bancario.banco;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Banco {
-    
+    private String nome;
+    private List<Conta> contas;
+
+    public Banco(String nome) {
+        this.nome = nome;
+        this.contas = new ArrayList<>();
+    }
+
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
+    }
+
+    public void removerConta(Conta conta) {
+        contas.remove(conta);
+    }
 }
+
