08792db8713a92a1351c5c80920c55a5e570e616
YASMINCOS <costayasmin367@gmail.com>
2024-04-17 20:07:00 -0300

    implementacao da logica da class cliente, nele e u precisei utilizar a Runnable, que é usada para representar a tarefa que pode ser executada em uma thread

--- sistema_bancario/pessoa/Cliente.java
+++ sistema_bancario/pessoa/Cliente.java
@@ -1,21 +1,37 @@
 package sistema_bancario.pessoa;
 
-import sistema_bancario.banco.Conta;
+import java.util.Random;
 
-public class Cliente  extends Pessoa{
-    
-    private Conta conta; 
+import sistema_bancario.banco.Conta;
+import sistema_bancario.loja.Loja;
+
+public class Cliente extends Pessoa implements Runnable {
+    private Conta conta;
 
     public Cliente(String nome, String endereco, String cpf, Conta conta) {
         super(nome, endereco, cpf);
         this.conta = conta;
     }
 
-    public Conta getConta() {
-        return conta;
+    @Override
+    public void run() {
+        Random random = new Random();
+        while (conta.getSaldo() > 0) {
+            int valorCompra = random.nextInt(2) == 0 ? 100 : 200; 
+            Loja loja = random.nextInt(2) == 0 ? new Loja("Loja 1") : new Loja("Loja 2");
+            realizarCompra(loja, valorCompra);
+        }
     }
 
-    public void setConta(Conta conta) {
-        this.conta = conta;
+    private void realizarCompra(Loja loja, double valor) {
+        synchronized (conta) {
+            if (conta.getSaldo() >= valor) {
+                conta.sacar(valor);
+                System.out.println(getNome() + " realizou uma compra de R$" + valor + " na " + loja.getName());
+            } else {
+                System.out.println(getNome() + " não possui saldo suficiente para realizar a compra de R$" + valor);
+            }
+        }
     }
 }
+
