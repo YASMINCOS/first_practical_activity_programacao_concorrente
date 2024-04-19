518296ada3b880528374399f146685646d652817
YASMINCOS <costayasmin367@gmail.com>
2024-04-19 14:30:11 -0300

    adicionando a logica de receber pagamento, usando lock

--- sistema_bancario/loja/Loja.java
+++ sistema_bancario/loja/Loja.java
@@ -1,5 +1,37 @@
 package sistema_bancario.loja;
 
+import sistema_bancario.banco.Banco;
+import sistema_bancario.banco.Conta;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Loja {
-    
+    private String nome;
+    private Banco banco;
+    private Conta conta;
+    private Lock lock;
+
+    public Loja(String nome, Banco banco, Conta conta) {
+        this.nome = nome;
+        this.banco = banco;
+        this.conta = conta;
+        this.lock = new ReentrantLock();
+    }
+
+    public String getName() {
+        return nome;
+    }
+
+    public void receberPagamento(double valor) {
+        lock.lock(); 
+        try {
+            conta.depositar(valor);
+            System.out.println("A loja " + nome + " recebeu um pagamento de R$" + valor);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+   
 }
